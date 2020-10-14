150379aed2558312bf1ec824e63e23170155bae0
Asad-Ur-Rahman <72619553+Asad-Ur-Rahman@users.noreply.github.com>
2020-10-14 11:54:57 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  const [isLit, setLit] = React.useState(false);
+  const [ isTemp, setTemp] = React.useState(72);
+  return (
+    <div className={`room ${isLit ? 'lit': 'dark'}`}>This room is {isLit ? 'lit': 'dark'}<br />
+    <button onClick={() => setLit(true)}>On</button>
+    <button onClick={() => setLit(false)}>Off</button><br />
+    <br /> Temperature is {isTemp}<br />
+    <button onClick={() => setTemp(isTemp + 1)}>+</button>
+     <button onClick={() => setTemp(isTemp - 1)}>-</button>
+
+    </div>
+
+    );
+}
+
+export default App;
